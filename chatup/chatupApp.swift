b6832e68ce9fafb7955c075158fe04d4d54767c2
namaya <n3amaya@gmail.com>
2023-09-08 21:06:01 -0500

    Initial Commit

---
+++ chatup/chatupApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatupApp.swift
+//  chatup
+//
+//  Created by Nicholas Amaya on 9/8/23.
+//
+
+import SwiftUI
+
+@main
+struct chatupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
