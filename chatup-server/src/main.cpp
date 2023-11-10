6d56d45316b1798f072c2fff5b1b66da391349ae
namaya <n3amaya@gmail.com>
2023-11-09 22:13:51 -0600

    feat(protos/): Added proto files to generate cpp server

--- chatup-server/src/main.cpp
+++ chatup-server/src/main.cpp
@@ -1,5 +1,34 @@
 
+#include "protobufs/chatup.grpc.pb.h"
 #include <grpcpp/grpcpp.h>
 #include <iostream>
+
+class ChatUpServerImpl final : public ChatUpServer::Service {
+
+    ::grpc::Status ListChatrooms(::grpc::ServerContext *context,
+                               const ::google::protobuf::Empty *request,
+                               ::Chatroom *response) {
+    return ::grpc::Status::OK;
+  }
+  ::grpc::Status CreateChatroom(::grpc::ServerContext *context,
+                                const ::Chatroom *request,
+                                ::Chatroom *response) {
+    return ::grpc::Status::OK;
+  }
+  ::grpc::Status Join(::grpc::ServerContext *context,
+                      const ::JoinRequest *request, ::ChatMessage *response) {
+    return ::grpc::Status::OK;
+  }
+  ::grpc::Status Leave(::grpc::ServerContext *context,
+                       const ::JoinRequest *request,
+                       ::google::protobuf::Empty *response) {
+    return ::grpc::Status::OK;
+  }
+  ::grpc::Status SendMessage(::grpc::ServerContext *context,
+                             const ::ChatMessage *request,
+                             ::google::protobuf::Empty *response) {
+    return ::grpc::Status::OK;
+  }
+};
 
 int main() { std::cout << "Hello, world!\n"; }
