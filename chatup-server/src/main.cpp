8308b5fb14f8beff85c150a4cd0cd9c5ad765fe5
namaya <n3amaya@gmail.com>
2023-11-11 00:34:00 -0600

    feat(CMakeLists.txt): Add skeleton gRPC server

--- chatup-server/src/main.cpp
+++ chatup-server/src/main.cpp
@@ -1,34 +1,47 @@
 
 #include "protobufs/chatup.grpc.pb.h"
-#include <grpcpp/grpcpp.h>
+#include <grpcpp/server_builder.h>
 #include <iostream>
 
 class ChatUpServerImpl final : public ChatUpServer::Service {
 
-    ::grpc::Status ListChatrooms(::grpc::ServerContext *context,
-                               const ::google::protobuf::Empty *request,
-                               ::Chatroom *response) {
-    return ::grpc::Status::OK;
+  grpc::Status ListChatrooms(grpc::ServerContext *context,
+                             const google::protobuf::Empty *request,
+                             Chatroom *response) {
+    return grpc::Status::OK;
   }
-  ::grpc::Status CreateChatroom(::grpc::ServerContext *context,
-                                const ::Chatroom *request,
-                                ::Chatroom *response) {
-    return ::grpc::Status::OK;
+  grpc::Status CreateChatroom(grpc::ServerContext *context,
+                              const Chatroom *request, Chatroom *response) {
+    return grpc::Status::OK;
   }
-  ::grpc::Status Join(::grpc::ServerContext *context,
-                      const ::JoinRequest *request, ::ChatMessage *response) {
-    return ::grpc::Status::OK;
+  grpc::Status Join(grpc::ServerContext *context, const JoinRequest *request,
+                    ChatMessage *response) {
+    return grpc::Status::OK;
   }
-  ::grpc::Status Leave(::grpc::ServerContext *context,
-                       const ::JoinRequest *request,
-                       ::google::protobuf::Empty *response) {
-    return ::grpc::Status::OK;
+  grpc::Status Leave(grpc::ServerContext *context, const ::JoinRequest *request,
+                     google::protobuf::Empty *response) {
+    return grpc::Status::OK;
   }
-  ::grpc::Status SendMessage(::grpc::ServerContext *context,
-                             const ::ChatMessage *request,
-                             ::google::protobuf::Empty *response) {
-    return ::grpc::Status::OK;
+  grpc::Status SendMessage(grpc::ServerContext *context,
+                           const ::ChatMessage *request,
+                           google::protobuf::Empty *response) {
+    return grpc::Status::OK;
   }
 };
 
-int main() { std::cout << "Hello, world!\n"; }
+int main() {
+  std::string server_address{"0.0.0.0:50051"};
+
+  ChatUpServerImpl grpcService{};
+
+  grpc::ServerBuilder builder{};
+
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&grpcService);
+
+  std::unique_ptr<grpc::Server> server{builder.BuildAndStart()};
+
+  std::cout << "Server listening on " << server_address << "\n";
+
+  server->Wait();
+}
